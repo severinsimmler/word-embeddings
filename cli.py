257812d99e9141d3ec2863b8b0cd7e5fdb1ccf2a
severinsimmler <severin.simmler@posteo.de>
2018-11-24 00:20:58 +0100

    feat: add parameters

--- cli.py
+++ cli.py
@@ -8,8 +8,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="matrix-tool",
                                      description="CLI tool to process a Wikipedia "
                                                  "dump to a word-word matrix.")
-    parser.add_argument('--corpus-dir', help="Path to corpus directory.")
+    parser.add_argument('--corpus', help="Path to corpus directory.")
+    parser.add_argument('--vocab', help="Path to vocabulary file.")
+    parser.add_argument('--window', help="Context window size.")
+    parser.add_argument('--output', help="Path to output file.")
 
     args = parser.parse_args()
-    
-    matrix = create_matrix(filepath, mfw, window_size, stopwords)
+
+    matrix = create_matrix(args["corpus_dir"],
+                           args["mfw"],
+                           args["window_size"],
+                           args["output"])
