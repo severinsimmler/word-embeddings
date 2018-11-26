9555b003c7b34c3327107672029d167661ef3eeb
maxnth <mail@maximiliannoeth.de>
2018-11-26 01:07:08 +0100

    feat: improved sparse matrix creation

--- matrix/utils.py
+++ matrix/utils.py
@@ -1,10 +1,10 @@
 import os
-import json
 import pickle
-import collections
 
 import nltk
 import regex as re
+import pandas as pd
+import scipy.sparse
 
 
 STOPWORDS = set(nltk.corpus.stopwords.words("german"))
@@ -19,26 +19,35 @@ def tokenize(text, pattern=r"\p{L}+\p{P}?\p{L}+"):
         yield match.group(0)
 
 
-def create_matrix(filepath, mfw, window_size, stopwords=STOPWORDS):
-    sparse_matrix = defaultdict(lambda: defaultdict(lambda: 0))
+def create_cooccurrence_matrix(filepath, mfw, window_size, stopwords=STOPWORDS):
+    voc = {}
+    row = []
+    col = []
+    data = []
     for root, dirs, files in os.walk(filepath):
         for file_ in files:
             with open(os.path.join(root, file_), "r", encoding="utf-8") as f:
-                data = f"[{f.read()[:-2]}]"
-                json_data = json.loads(data)
-                for row in json_data:
-                    for sentence in nltk.tokenize.sent_tokenize(row["text"]):
-                        tokens = list(tokenize(sentence.lower()))
-                        for token in tokens:
-                            if token in mfw:
-                                for i in [x for x in range(-window_size, window_size + 1) if x != 0]:
-                                    if tokens.index(token) + i >= 0:
-                                        try:
-                                            if tokens[tokens.index(token) + i] not in stopwords:
-                                                sparse_matrix[token][tokens[tokens.index(token) + i]] += 1
-                                        except IndexError:
-                                            pass
-                                        continue
+                sentences = nltk.sent_tokenize(f.read())
+                for sentence in sentences:
+                    tokens = [token.lower() for token in tokenize(sentence)]
+                    for pos, token in enumerate(tokens):
+                        if token in stopwords or token not in mfw:
+                            continue
+                        i = voc.setdefault(token, len(voc))
+                        start = max(0, pos-window_size)
+                        end = min(len(tokens), pos+window_size+1)
+                        for pos2 in range(start, end):
+                            if pos2 == pos or tokens[pos2] in stopwords or tokens[pos2] not in mfw:
+                                continue
+                            j = voc.setdefault(tokens[pos2], len(voc))
+                            data.append(1)
+                            row.append(i)
+                            col.append(j)
+    csr = sparse.coo_matrix((data, (row, col))).tocsr()
+    df = pd.SparseDataFrame(csr)
+    voc = dict((v, k) for k, v in voc.items())
+    df = df.rename(index=voc); df = df.rename(columns=voc)
+    return df
 
 
 def save_matrix(matrix, filepath):
