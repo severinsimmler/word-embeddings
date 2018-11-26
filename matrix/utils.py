113e667bef76c78e9b2905a97b2c60d04bd6312f
severinsimmler <severin.simmler@posteo.de>
2018-11-26 20:43:47 +0100

    feat: load mfw

--- matrix/utils.py
+++ matrix/utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 import collections
 
@@ -25,6 +26,9 @@ def create_frequency_list(filepath, limit, stopwords=STOPWORDS):
 
 
 def create_cooccurrence_matrix(filepath, mfw, window_size, stopwords=STOPWORDS):
+    if isinstance(mfw, str):
+        with open(mfw, "r", encoding="utf-8") as f:
+            mfw = json.load(f)
     voc = {}
     row = []
     col = []
@@ -32,22 +36,26 @@ def create_cooccurrence_matrix(filepath, mfw, window_size, stopwords=STOPWORDS):
     for root, dirs, files in os.walk(filepath):
         for file_ in files:
             with open(os.path.join(root, file_), "r", encoding="utf-8") as f:
-                sentences = nltk.sent_tokenize(f.read())
-                for sentence in sentences:
-                    tokens = [token.lower() for token in tokenize(sentence)]
-                    for pos, token in enumerate(tokens):
-                        if token in stopwords or token not in mfw:
-                            continue
-                        i = voc.setdefault(token, len(voc))
-                        start = max(0, pos-window_size)
-                        end = min(len(tokens), pos+window_size+1)
-                        for pos2 in range(start, end):
-                            if pos2 == pos or tokens[pos2] in stopwords or tokens[pos2] not in mfw:
+                # Does not load the whole file:
+                for line in f:
+                    sentences = nltk.sent_tokenize(line)
+                    for sentence in sentences:
+                        # Lowering _one_ string is more efficient than lowering _n_ tokens _n_ times:
+                        tokens = [token for token in tokenize(sentence.lower())]
+                        for pos, token in enumerate(tokens):
+                            if token in stopwords or token not in mfw:
                                 continue
-                            j = voc.setdefault(tokens[pos2], len(voc))
-                            data.append(1)
-                            row.append(i)
-                            col.append(j)
+                            i = voc.setdefault(token, len(voc))
+                            start = max(0, pos-window_size)
+                            end = min(len(tokens), pos+window_size+1)
+                            for pos2 in range(start, end):
+                                if pos2 == pos or tokens[pos2] in stopwords or tokens[pos2] not in mfw:
+                                    continue
+                                j = voc.setdefault(tokens[pos2], len(voc))
+                                data.append(1)
+                                row.append(i)
+                                col.append(j)
+                    break
     csr = scipy.sparse.coo_matrix((data, (row, col))).tocsr()
     df = pd.SparseDataFrame(csr)
     voc = dict((v, k) for k, v in voc.items())
